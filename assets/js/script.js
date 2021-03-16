ceb1fc8d992121b795e90c35f26d7292cb25c9ad
DAVE PEYTON <davepeyton@DAVEs-MacBook-Pro.local>
2021-03-16 19:05:01 +0000

    add js game  const

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const yodas = document.querySelectorAll('.yoda');
+const countdownBoard = document.querySelector('.startButton');
