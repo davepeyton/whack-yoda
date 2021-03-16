159bb051400b902f7fb436f4f95a67d40c395961
DAVE PEYTON <davepeyton@DAVEs-MacBook-Pro.local>
2021-03-16 19:10:08 +0000

    added js let vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const yodas = document.querySelectorAll('.yoda');
 const countdownBoard = document.querySelector('.startButton');
+
+let lastHole;
+let timeUp = false;
+let timeLimit = 10000;
+let score = 0;
+let countdown;
